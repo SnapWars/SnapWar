b719709bee808c11c9bfe6ab0d4dc27c1ac31309
misspia <pia.leung@mediamonks.com>
2020-04-23 21:12:25 +0200

    Move form to create battle view, separate textfield delegate methods out with an extension

--- SnapWar/ViewControllers/SWAuthViewController.swift
+++ SnapWar/ViewControllers/SWAuthViewController.swift
@@ -2,9 +2,6 @@ import UIKit
 
 class SWAuthViewController: UIViewController, UITextFieldDelegate {
     let formContainer = UIStackView()
-    var usernameField = SWTextField()
-    let passwordField = SWPasswordTextField()
-    let loginButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,67 +25,5 @@ class SWAuthViewController: UIViewController, UITextFieldDelegate {
         formContainer.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
         formContainer.isLayoutMarginsRelativeArrangement = true
         formContainer.layoutMargins = UIEdgeInsets(top: 0, left: horizontalMargin, bottom: 0, right: horizontalMargin)
-        
-        usernameField.placeholder = "username"
-        usernameField.delegate = self
-        usernameField.tag = 0
-        formContainer.addArrangedSubview(usernameField)
-
-        passwordField.placeholder = "password"
-        passwordField.delegate = self
-        passwordField.tag = 1
-        formContainer.addArrangedSubview(passwordField)
-        
-        loginButton.setTitle("Login", for: .normal)
-        loginButton.backgroundColor = SWColorType.primary
-        loginButton.addTarget(self, action: #selector(handleSubmit), for: .touchUpInside)
-        setLoginButtonState()
-        formContainer.addArrangedSubview(loginButton)
     }
-    
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        setLoginButtonState()
-    }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        let nextTag = textField.tag + 1
-        
-        if let nextResponder = textField.superview?.viewWithTag(nextTag) {
-            nextResponder.becomeFirstResponder()
-        } else {
-            textField.resignFirstResponder()
-        }
-        
-        return true
-    }
-    
-    func isFormValid() -> Bool {
-        if let isUsernameEmpty = usernameField.text?.isEmpty {
-            if isUsernameEmpty {
-                return false
-            }
-        }
-        
-        if let isPasswordEmpty = passwordField.text?.isEmpty {
-            if isPasswordEmpty {
-                return false
-            }
-        }
-        return true
-    }
-    
-    func setLoginButtonState() {
-        loginButton.isEnabled = isFormValid()
-        if loginButton.isEnabled {
-            loginButton.alpha = 1.0
-        } else {
-            loginButton.alpha = 0.5
-        }
-    }
-    
-    @objc
-    func handleSubmit() {
-
-    }
-    
 }

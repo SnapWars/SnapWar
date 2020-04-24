007b7d4b950dd317e82d6c1b080315f5de14e7e8
misspia <pia.leung@mediamonks.com>
2020-04-24 23:56:27 +0200

    Remove pods directory from git

--- Pods/Target Support Files/Pods-SnapWar/Pods-SnapWar-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SnapWar : NSObject
-@end
-@implementation PodsDummy_Pods_SnapWar
-@end
--- Pods/Target Support Files/Pods-SnapWarShareExtension/Pods-SnapWarShareExtension-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SnapWarShareExtension : NSObject
-@end
-@implementation PodsDummy_Pods_SnapWarShareExtension
-@end

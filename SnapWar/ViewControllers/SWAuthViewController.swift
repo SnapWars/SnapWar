5658f11d14d6feab9b97decc1e8dc70df1325b75
misspia <pia.leung@mediamonks.com>
2020-05-01 00:19:51 +0200

    Create user entity codeable

--- SnapWar/ViewControllers/SWAuthViewController.swift
+++ SnapWar/ViewControllers/SWAuthViewController.swift
@@ -42,23 +42,65 @@ class SWAuthViewController: UIViewController {
         })
     }
     
+//    fileprivate func fetchUserInfo() {
+//        let graphQLQuery = "{me{displayName, bitmoji{avatar}, externalId}}"
+//
+//        SCSDKLoginClient.fetchUserData(
+//                withQuery: graphQLQuery,
+//                variables: nil,
+//                success: { userInfo in
+//                    if let userInfo = userInfo,
+//                       let jsonData = try? JSONSerialization.data(withJSONObject: userInfo, options: .prettyPrinted) {
+//                        if let data = String(data: jsonData, encoding: .ascii) {
+//                            print("Fetched user info: " + data)
+//                            // TODO: Assign data to user
+//
+//                        }
+//                    }
+//            }) { (error, isUserLoggedOut) in
+//                print(error?.localizedDescription ?? "")
+//        }
+//    }
     fileprivate func fetchUserInfo() {
-        let graphQLQuery = "{me{displayName, bitmoji{avatar}, externalId}}"
-        
-        SCSDKLoginClient.fetchUserData(
-                withQuery: graphQLQuery,
-                variables: nil,
-                success: { userInfo in
-                    if let userInfo = userInfo,
-                       let jsonData = try? JSONSerialization.data(withJSONObject: userInfo, options: .prettyPrinted) {
-                        if let data = String(data: jsonData, encoding: .ascii) {
-                            print("Fetched user info: " + data)
-                            // TODO: Assign data to user
-                           
+        print("fetchUserInfo")
+           let graphQLQuery = "{me{displayName, bitmoji{avatar}, externalId}}"
+           
+           SCSDKLoginClient.fetchUserData(
+                   withQuery: graphQLQuery,
+                   variables: nil,
+                   success: { userInfo in
+                       if let userInfo = userInfo,
+                        let data = try? JSONSerialization.data(withJSONObject: userInfo, options: .prettyPrinted) {
+ 
+                        let userEntity = try? JSONDecoder().decode(UserEntity.self, from: data)
+                        print("??????",  userEntity)
+                        
+                        
+                        
                         }
-                    }
-            }) { (error, isUserLoggedOut) in
-                print(error?.localizedDescription ?? "")
+               }) { (error, isUserLoggedOut) in
+                   print(error?.localizedDescription ?? "")
         }
+    }
+    
+    fileprivate func goToLoginConfirm(_ userEntity: Any) {
+        print("goToLoginConfirm", userEntity)
+    }
+}
+
+struct UserEntity: Codable {
+    let data: [String: String]
+    let me: [String: String]
+    let bitmoji: [String: String]
+    let avatar: String
+    let name: String
+    
+    enum CodingKeys: String, CodingKey {
+        case data = "data"
+        case me = "me"
+        case bitmoji = "bitmoji"
+        case name = "displayName"
+        case avatar = "avatar"
+        
     }
 }

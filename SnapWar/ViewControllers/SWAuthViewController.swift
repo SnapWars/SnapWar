2e83628c6f763e6369568bb5813c31202c3456d5
David Zhang <davidozhang@gmail.com>
2020-05-01 00:32:58 -0700

    Extract avatar url, display name and external id from LoginKit response

--- SnapWar/ViewControllers/SWAuthViewController.swift
+++ SnapWar/ViewControllers/SWAuthViewController.swift
@@ -42,25 +42,6 @@ class SWAuthViewController: UIViewController {
         })
     }
     
-//    fileprivate func fetchUserInfo() {
-//        let graphQLQuery = "{me{displayName, bitmoji{avatar}, externalId}}"
-//
-//        SCSDKLoginClient.fetchUserData(
-//                withQuery: graphQLQuery,
-//                variables: nil,
-//                success: { userInfo in
-//                    if let userInfo = userInfo,
-//                       let jsonData = try? JSONSerialization.data(withJSONObject: userInfo, options: .prettyPrinted) {
-//                        if let data = String(data: jsonData, encoding: .ascii) {
-//                            print("Fetched user info: " + data)
-//                            // TODO: Assign data to user
-//
-//                        }
-//                    }
-//            }) { (error, isUserLoggedOut) in
-//                print(error?.localizedDescription ?? "")
-//        }
-//    }
     fileprivate func fetchUserInfo() {
         print("fetchUserInfo")
            let graphQLQuery = "{me{displayName, bitmoji{avatar}, externalId}}"
@@ -69,15 +50,31 @@ class SWAuthViewController: UIViewController {
                    withQuery: graphQLQuery,
                    variables: nil,
                    success: { userInfo in
-                       if let userInfo = userInfo,
-                        let data = try? JSONSerialization.data(withJSONObject: userInfo, options: .prettyPrinted) {
- 
-                        let userEntity = try? JSONDecoder().decode(UserEntity.self, from: data)
-                        print("??????",  userEntity)
-                        
-                        
-                        
+                    /**
+                     Structure:
+                     ["me": {
+                         bitmoji =     {
+                             avatar = "link";
+                         };
+                         displayName = "name";
+                         externalId = "id";
+                     }]
+                     */
+                    if let userInfo = userInfo,
+                       let jsonData = try? JSONSerialization.data(withJSONObject: userInfo, options: .prettyPrinted),
+                       let dict = try? JSONSerialization.jsonObject(with: jsonData, options: []) as? [String: Any],
+                       let data = dict["data"] as? [String: Any],
+                       let me = data["me"] as? [String: Any] {
+                        if let bitmoji = me["bitmoji"] as? [String: Any],
+                            let avatarURL = bitmoji["avatar"] as? String {
+                            // Use avatar URL
                         }
+
+                        if let displayName = me["displayName"] as? String,
+                           let externalId = me["externalId"] as? String {
+                            // Use display name, external id
+                        }
+                    }
                }) { (error, isUserLoggedOut) in
                    print(error?.localizedDescription ?? "")
         }
